a1c096b6072f890f7c02d4f4f361e0f64ca91f7b
AZEEM ASLAM <azeemaslam136@gmail.com>
2026-05-16 19:41:13 +0530

    render ki wajah commit

--- server.js
+++ server.js
@@ -21,6 +21,17 @@ const memoryStore = {
 };
 let memoryIdCounter = 1;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(204);
+    return;
+  }
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
